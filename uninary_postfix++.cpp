25d033b37772eaef840be8e1c1436667364d0ec5
kalyan1412 <47789050+kalyan1412@users.noreply.github.com>
2021-06-11 20:54:18 +0530

    Create uninary_postfix++.cpp

---
+++ uninary_postfix++.cpp
@@ -0,0 +1,28 @@
+
+
+// uninary prefix operator
+
+#include <iostream>
+
+
+class Sample {
+    private:
+    int x, y;
+    public:
+    Sample () { }
+    Sample (int a) : x(a) { }
+    void display() {
+        std::cout << x << std::endl;
+    }
+    Sample operator++(int a) {
+        this->x++;
+        return *this;
+    }
+};
+
+int main() {
+    Sample s1 (11);
+    s1++;
+    s1.display();
+    return 0;
+}
