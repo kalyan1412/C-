537cdd6f2dd55195524c0d4f58d93f618c7c4b58
kalyan1412 <47789050+kalyan1412@users.noreply.github.com>
2021-08-26 11:11:44 +0530

    Create why_mutex.cpp

---
+++ why_mutex.cpp
@@ -0,0 +1,26 @@
+// why we need mutex
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+int x = 0; // global variable
+
+std::mutex m;
+
+void fun () {
+	for (int i =0; i < 200000; i++) {
+		m.lock();
+		x++;
+		m.unlock();
+	}
+}
+
+int main() {
+	std::thread t1 (fun);
+	std::thread t2 (fun);
+	t1.join();
+	t2.join();
+	std::cout << x << std::endl;
+	return 0;
+}
